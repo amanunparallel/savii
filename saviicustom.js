63f39ac4f443ec9bec6f43b4329849a5e997965c
amanunparallel <110657798+amanunparallel@users.noreply.github.com>
2023-07-31 09:55:14 +0530

    Update saviicustom.js

--- saviicustom.js
+++ saviicustom.js
@@ -372,3 +372,32 @@ if (textEarningSix) {
   amount.addEventListener("input", updateEarnings);
   time.addEventListener("input", updateEarnings);
 }
+
+
+// JavaScript code
+let tabsComponents = document.querySelectorAll("[products='tab']");
+
+if (tabsComponents.length > 0) {
+  function tabsfunction(tab) {
+    let productTablinks = tab.querySelectorAll("[products='tablink']");
+    let productTabContent = tab.querySelectorAll("[products='tab-content']");
+
+    // Add click event listener to each tab link
+    productTablinks.forEach((tablink, index) => {
+      tablink.addEventListener("click", () => {
+        // Remove "active" class from all tab links and tab contents
+        productTablinks.forEach((tl) => tl.classList.remove("active"));
+        productTabContent.forEach((tc) => tc.classList.remove("active"));
+
+        // Add "active" class to the clicked tab link and its corresponding tab content
+        tablink.classList.add("active");
+        productTabContent[index].classList.add("active");
+      });
+    });
+  }
+
+  tabsComponents.forEach((tab) => {
+    tabsfunction(tab);
+  });
+}
+
